e3df2609d8e06e75f65ce3d82883e06c81f64fce
René Abreu <rene.abreu@outlook.com>
2019-05-25 13:52:30 -0400

    added table component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Breadcrumb from './components/common/menu/Breadcrumb';
 // Home
 import Welcome from './components/sections/home/Welcome';
 import CounterCard from './components/sections/home/CounterCard';
+import Table from './components/sections/home/Table'
 
 function App() {
   return (
@@ -44,55 +45,7 @@ function App() {
         </div>
             
             {/* TABLE */}
-          <div className="columns">
-            <div className="column is-12">
-              <div className="card events-card">
-
-                <header className="card-header">
-                    <p className="card-header-title">Últimas Ordenes</p>
-                    <a href="#" className="card-header-icon" aria-label="more options">
-                        <span className="icon">
-                        <i className="fa fa-angle-down" aria-hidden="true"></i>
-                        </span>
-                    </a>
-                </header>
-
-                <div className="card-table">
-                  <div className="content">
-                  <table className="table is-fullwidth is-striped">
-                    <tbody>
-                        <tr>
-                            <td width="5%"><i className="fa fa-bell-o"></i></td>
-                            <td>Lorum ipsum dolem aire</td>
-                            <td><a className="button is-small is-primary" href="#">Editar</a></td>
-                        </tr>
-                        <tr>
-                            <td width="5%"><i className="fa fa-bell-o"></i></td>
-                            <td>Lorum ipsum dolem aire</td>
-                            <td><a className="button is-small is-primary" href="#">Editar</a></td>
-                        </tr>
-                        <tr>
-                            <td width="5%"><i className="fa fa-bell-o"></i></td>
-                            <td>Lorum ipsum dolem aire</td>
-                            <td><a className="button is-small is-primary" href="#">Editar</a></td>
-                        </tr>
-                        <tr>
-                            <td width="5%"><i className="fa fa-bell-o"></i></td>
-                            <td>Lorum ipsum dolem aire</td>
-                            <td><a className="button is-small is-primary" href="#">Editar</a></td>
-                        </tr>  
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-
-                <footer className="card-footer">
-                    <a href="#" className="card-footer-item">View All</a>
-                </footer>
-
-              </div>
-            </div>
-          </div>
+            <Table />
         </div>
   
   
