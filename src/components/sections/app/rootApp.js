040128c4b4374b9e5e3d8edce956c54057c3900b
René Abreu <rene.abreu@outlook.com>
2019-05-25 14:50:33 -0400

    Added routes

--- src/components/sections/app/rootApp.js
+++ src/components/sections/app/rootApp.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import style from './rootApp.module.css';
+
+import Navbar from '../../common/menu/Navbar'
+import Home from '../home/Home'
+import Orders from '../orders/Orders'
+import Services from '../services/Services'
 
 class App extends Component {
     render() {
 
         return (
-            <h1>FUNCIONA</h1>
-        )
+            <div className={style.container}>
+                <Navbar/>
+                {/* <h1>FUNCIONA</h1> */}
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/orders' component={Orders} />
+                        <Route exact path='/services' component={Services} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+       )
     }
 }
 
