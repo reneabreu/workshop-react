170ca9c1d8b91491808060c5ea88a2b2a619b8f1
René Abreu <rene.abreu@outlook.com>
2019-05-25 14:34:50 -0400

    changing rootApp to be first page and starting routes

---
+++ src/components/sections/app/rootApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+    render() {
+
+        return (
+            <h1>FUNCIONA</h1>
+        )
+    }
+}
+
+export default App;
